0bb7848b1455ee08091dde53d8d0f64dbeab28f3
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-01-27 18:49:30 -0500

    Create Game State
    Game state is used for taking data from main menu and using it to initialize the players. It can probably store more data, and when the game gets more complex, playerdata may need to get more complex.

---
+++ Time Gate/Assets/Scripts/GameState.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public static readonly string FLAG_MULTIPLAYER = "Multiplayer";
+    public static readonly string FLAG_PLAYER_ONE = "P1";
+    public static readonly string FLAG_PLAYER_TWO = "P2";
+    public static readonly string FLAG_PLAYER_THREE = "P3";
+    public static readonly string FLAG_PLAYER_FOUR = "P4";
+    //false is set to zero because the playerprefs defaults to zero if the flag hasnt been set, assume false unless
+    //told otherwise.
+    public static readonly int FLAG_VALUE_TRUE = 1;
+    public static readonly int FLAG_VALUE_FALSE = 0;
+
+    //references to the players
+    public PlayerData[] players;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestMultiplayer();
+        //TestSinglePlayer();
+        InitializePlayers();
+    }
+
+    void InitializePlayers()
+    {
+        //detect if singleplayer
+        if(PlayerPrefs.GetInt(FLAG_MULTIPLAYER) == FLAG_VALUE_FALSE)
+        {
+            Debug.Log("Running");
+            //set player with id player num zero to active, activate object
+            PlayerData player = players[0];
+            player.isActive = true;
+            player.gameObject.SetActive(true);
+            PlayerDataCollection.instance.AddPlayer(player, player.playerID);
+
+            //activate corresponding ui.
+        }
+        else
+        {
+            //check each player to see if active(Player one must be active)
+            //set player with id player num zero to active, activate object
+            PlayerData player1 = players[0];
+            player1.isActive = true;
+            player1.gameObject.SetActive(true);
+            PlayerDataCollection.instance.AddPlayer(player1, player1.playerID);
+
+            //activate corresponding ui.
+            if (PlayerPrefs.GetInt(FLAG_PLAYER_TWO) == FLAG_VALUE_TRUE)
+            {
+                PlayerData player = players[1];
+                player.isActive = true;
+                player.gameObject.SetActive(true);
+                PlayerDataCollection.instance.AddPlayer(player, player.playerID);
+            }
+
+            if (PlayerPrefs.GetInt(FLAG_PLAYER_THREE) == FLAG_VALUE_TRUE)
+            {
+                PlayerData player = players[2];
+                player.isActive = true;
+                player.gameObject.SetActive(true);
+                PlayerDataCollection.instance.AddPlayer(player, player.playerID);
+            }
+
+            if (PlayerPrefs.GetInt(FLAG_PLAYER_FOUR) == FLAG_VALUE_TRUE)
+            {
+                PlayerData player = players[3];
+                player.isActive = true;
+                player.gameObject.SetActive(true);
+                PlayerDataCollection.instance.AddPlayer(player, player.playerID);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TestUserPrefs() {
+        PlayerPrefs.SetInt(FLAG_MULTIPLAYER,FLAG_VALUE_TRUE);
+        Debug.Log(PlayerPrefs.GetInt(FLAG_MULTIPLAYER));
+    }
+
+    void ResetFlags()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    void TestNotSet()
+    {
+        //If a value is not set, it assumes it is zero
+        Debug.Log(PlayerPrefs.GetInt("Some value"));
+    }
+
+    void TestMultiplayer()
+    {
+        PlayerPrefs.SetInt(FLAG_MULTIPLAYER, FLAG_VALUE_TRUE);
+        Debug.Log("Multiplayer: " +  PlayerPrefs.GetInt(FLAG_MULTIPLAYER));
+        PlayerPrefs.SetInt(FLAG_PLAYER_ONE, FLAG_VALUE_TRUE);
+        Debug.Log("Player 1: " + PlayerPrefs.GetInt(FLAG_PLAYER_ONE));
+        PlayerPrefs.SetInt(FLAG_PLAYER_TWO, FLAG_VALUE_TRUE);
+        Debug.Log("Player 2: " + PlayerPrefs.GetInt(FLAG_PLAYER_TWO));
+        PlayerPrefs.SetInt(FLAG_PLAYER_THREE, FLAG_VALUE_TRUE);
+        Debug.Log("Player 3: " + PlayerPrefs.GetInt(FLAG_PLAYER_THREE));
+        PlayerPrefs.SetInt(FLAG_PLAYER_FOUR, FLAG_VALUE_FALSE);
+        Debug.Log("Player 4: " + PlayerPrefs.GetInt(FLAG_PLAYER_FOUR));
+    }
+
+    void TestSinglePlayer()
+    {
+        PlayerPrefs.SetInt(FLAG_MULTIPLAYER, FLAG_VALUE_FALSE);
+        Debug.Log("Multiplayer: " + PlayerPrefs.GetInt(FLAG_MULTIPLAYER));
+    }
+}
