b004c061387e5196b420e16c3772280d6d3c80d2
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-01-29 14:00:01 -0500

    Added SpawnEnemy Script
    It does not actually spawn the enemy, so the name should be refactored, but it does house the collider for detecting if it is safe to spawn, and the transform for the location the enemies should spawn at.

---
+++ Time Gate/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    // flag saying that the spawn is available
+    public bool isAvailable = true;
+    //public BoxCollider2D collider;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isAvailable = false;
+        Debug.Log("Someone entered");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isAvailable = true;
+        Debug.Log("Someone left");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isAvailable = false;
+        Debug.Log("Someone stayed");
+    }
+}
