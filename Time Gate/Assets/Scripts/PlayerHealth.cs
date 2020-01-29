51b7c5e54431aae8985612a050661b9716974457
Sean Santorufo <seansantorufo@gmail.com>
2020-01-29 17:52:59 -0500

    Health script
    Started a health script and got a basic one to function with a slider acting as the health bar

---
+++ Time Gate/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float MaxHealth { get; set; }
+    public float Health { get; set; }
+    public Slider playerHealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //establishes health bar at start of game
+        MaxHealth = 100f;
+        Health = MaxHealth;
+        playerHealthBar.value = CalculateHealth();
+    }
+
+    //decreases health based on damage amount
+    void Damage(float damageAmount)
+    {
+        Health -= damageAmount;
+        playerHealthBar.value = CalculateHealth();
+
+        //Will also handle if player is dead once health is 0
+    }
+
+    //calculates current health 
+    float CalculateHealth()
+    {
+        return Health / MaxHealth;
+    }
+
+    //This is where denemy collision damaged will be handled
+    void Update()
+    {
+        //for now, a key input represents how damage would work
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Damage(5);
+        }
+    }
+}
