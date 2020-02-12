d7ad109e0a866df292b37ed35ee1e541b650c6f3
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-02-12 10:29:28 -0500

    Update PlayerData.cs
    Handles knockback, updates data for being knocked back.

--- Time Gate/Assets/Scripts/PlayerData.cs
+++ Time Gate/Assets/Scripts/PlayerData.cs
@@ -11,12 +11,15 @@ public class PlayerData : MonoBehaviour
     [Range(1,100)]
     public int maxPlayerHealth;
     private int playerScore;
+    public Rigidbody2D rb;
+    private bool knocked;
     // Start is called before the first frame update
     void Awake()
     {
         isActive = transform.gameObject.activeSelf;
         playerHealth = maxPlayerHealth;
         playerScore = 0;
+        knocked = false;
     }
 
     // Update is called once per frame
@@ -39,12 +42,33 @@ public class PlayerData : MonoBehaviour
             Damage(collision.gameObject.GetComponent<DroneAIBeta2>().GetContactDamage());
 
             //knock back on enemies.
+            //calculate angle
+            Vector3 direction = rb.transform.position - collision.gameObject.transform.position;
+            if (!knocked)
+            {
+                StartCoroutine(Knockback(direction));
+            }
+            
         }
         else if (collision.gameObject.CompareTag("enemy_projectile"))
         {
             //get the damage from it, and damage
             Damage(collision.gameObject.GetComponent<ProjectileDamage>().GetDamage());
             Destroy(collision.gameObject);
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Vector2 point = collision.contacts[0].point;
+            Vector2 direction = new Vector2( rb.transform.position.x, rb.transform.position.y)-point;
+            if (!knocked)
+            {
+                StartCoroutine(Knockback(direction));
+            }
         }
     }
 
@@ -55,6 +79,21 @@ public class PlayerData : MonoBehaviour
         {
             Die();
         }
+    }
+
+    public IEnumerator Knockback(Vector3 direction)
+    {
+        knocked = true;
+        //rb.AddForce(new Vector2(direction.normalized.x,
+        //        direction.normalized.x) * 6000);//TODO: mess around with values.
+        rb.velocity = direction * 10;
+        yield return new WaitForSeconds(0.1f);
+        knocked = false;
+    }
+
+    public bool isKnocked()
+    {
+        return knocked;
     }
 
     void Die()
