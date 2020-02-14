1572b97a38493449bc399f5752960ecf61fec310
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-02-13 21:51:07 -0500

    Create Dialog Script
    This is the one dialog box on the screen, and NPC's initialize all of the data. I need to figure out how to update the data on it without timescale.

---
+++ Time Gate/Assets/Scripts/Dialog.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    public Text NPCName, NPCText;
+    public GameObject NPCPicture;
+    //public Script script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetName(string name)
+    {
+        NPCName.text = name;
+    }
+
+    public void UpdateText(string text)
+    {
+        NPCText.text = text;
+        //TODO - cool animation
+    }
+
+    public void SetImage(GameObject obj)
+    {
+        int childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++) {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        Instantiate(obj, NPCPicture.transform);
+
+    }
+
+    public void initialize()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void dissapear()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+
+}
