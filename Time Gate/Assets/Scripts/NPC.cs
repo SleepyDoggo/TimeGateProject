9552b10799ba84131c26bc478fe7a72041fa55bf
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-02-13 19:51:56 -0500

    Created NPC script
    This script will contain information for the information necessary for the dialog box to exist. It will be part of the UI that is hidden. It will need a prefab above it to show as the character, as well as some data, like a name. This will be provided by the NPC data, which will store a prefab for the character image, a name, and a script.

---
+++ Time Gate/Assets/Scripts/NPC.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private bool approached;
+    private int playerNum;
+    [Range(0,3)]
+    public int targetDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        approached = false;
+        playerNum = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if the player with the playernum has pressed the interact button, only
+        //if they have been approached.
+        if (approached)
+        {
+            if(Input.GetButtonDown("Player" + (playerNum+1) + "AButton") || Input.GetKeyDown("space"))
+            {
+                //start the dialog
+                Debug.Log("The dialog should be triggered");
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if the thing is a player, get their player number
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (!approached)
+            {
+                //also check if the side it is coming from is correct.
+                Vector2 point = collision.contacts[0].point;
+                Vector2 direction = new Vector2(collision.gameObject.transform.position.x,
+                    collision.gameObject.transform.position.y) - 
+                    new Vector2(transform.position.x, transform.position.y);
+                if (IsRightDirection(direction))
+                {
+                    approached = true;
+                    Debug.Log(approached);
+                    playerNum = collision.gameObject.GetComponent<PlayerData>().playerID;
+                }
+                
+            }
+        }
+    }
+
+    private bool IsRightDirection(Vector2 direction)
+    {
+        int correctAngle = targetDirection * 90;
+        float angle = -Vector2.SignedAngle(direction.normalized, Vector2.right);
+        angle = (angle + 360) % 360;
+        //Debug.Log("Angle: " + angle + " Target: " + correctAngle);
+        //0 is a special direction
+        if(targetDirection == 0)
+        {
+            if ((angle > 315 && angle < 360) || (angle > 0 && angle < 45))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        else if (angle > correctAngle - 45 && angle < correctAngle + 45)
+        {
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 point = collision.contacts[0].point;
+            Vector2 direction = new Vector2(collision.gameObject.transform.position.x,
+                collision.gameObject.transform.position.y) -
+                new Vector2(transform.position.x, transform.position.y);
+            if (IsRightDirection(direction))
+            {
+                approached = true;
+                playerNum = collision.gameObject.GetComponent<PlayerData>().playerID;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        approached = false;
+    }
+}
+
