7eed8ae397493bfafa73e58802e89982cc4d908b
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-03-02 15:32:10 -0500

    Creates rift spawn script.
    This script creates a bunch of rifts. This will spawn the rifts in their correct locations. I will update the script to get the amount of rifts needed from a state tracker, which will detect when the rifts are destroyed.

---
+++ Time Gate/Assets/Scripts/RiftSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiftSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] spawnPoints;
+    public int numRifts;
+    public GameObject rift;
+    void Start()
+    {
+        //put a rift into the number of rifts specified, pick randomly from spawnPoints;
+
+        //safety, avoid possible null pointer exceptions
+        if (numRifts > spawnPoints.Length) {
+            numRifts = spawnPoints.Length;
+        }
+
+        int[] indeces = FindIndeces();
+        SpawnRifts(indeces);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int[] FindIndeces()
+    {
+        int[] indeces = new int[numRifts];
+        bool[] indecesUsed = new bool[spawnPoints.Length];
+        for(int i=0; i < indeces.Length; i++) {
+            int spawnIndex;
+            do
+            {
+                spawnIndex = Random.Range(0, spawnPoints.Length);
+            } while (indecesUsed[spawnIndex]);
+            indecesUsed[spawnIndex] = true;
+            indeces[i] = spawnIndex;
+        }
+        return indeces;
+    }
+
+    void SpawnRifts(int[] indeces) {
+        foreach (int index in indeces) {
+            SpawnRift(index);
+        }
+    }
+
+    void SpawnRift(int index)
+    {
+        GameObject tmp = Instantiate(rift, spawnPoints[index].transform);
+        tmp.transform.position = spawnPoints[index].transform.position;
+    }
+}
