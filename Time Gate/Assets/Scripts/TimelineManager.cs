9af72b94307a2a1f059b9ee049efb4ddf85feb6f
SleepyDoggo <akashjad1998@gmail.com>
2020-03-22 23:00:28 -0400

    Timeline Manager Script
    This fixes movement after a cutscene since there players are usually stuck in place after a cutscene occurs.

---
+++ Time Gate/Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+//currently this just fixes the movement after cutscene for player.
+public class TimelineManager : MonoBehaviour
+{
+    private bool fix = false;
+    public Animator playerAnimator;
+    public RuntimeAnimatorController playerAnim;
+    public PlayableDirector director;
+    // Start is called before the first frame update
+    void OnEnable() //this is for trigger zones. Could also just change to Start()
+    {
+        playerAnim = playerAnimator.runtimeAnimatorController;
+        playerAnimator.runtimeAnimatorController = null;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(director.state != PlayState.Playing && !fix)
+        {
+            fix = true;
+            playerAnimator.runtimeAnimatorController = playerAnim;
+        }
+    }
+}
