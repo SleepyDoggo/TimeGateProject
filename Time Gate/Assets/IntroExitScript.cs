bd5905d5e93c8e873322943433404ef01c913e76
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-03-22 20:13:19 -0400

    Workin on it
    There were several props added to the scene, so this is a lot of sprites.

---
+++ Time Gate/Assets/IntroExitScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroExitScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //double check tag is correct
+        if (collision.gameObject.CompareTag("player"))
+        {
+            //perform exit logic
+
+        }
+    }
+}
