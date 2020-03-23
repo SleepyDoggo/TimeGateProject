ab11ae992c6015ed97867a3f419427f3b1694517
Michael Pascale <michaelpascal3557@gmail.com>
2020-03-22 23:26:33 -0400

    Fixed the portal
    Collisions now work.

--- Time Gate/Assets/IntroExitScript.cs
+++ Time Gate/Assets/IntroExitScript.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class IntroExitScript : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Collidiing");
         //double check tag is correct
         if (collision.gameObject.CompareTag("Player"))
         {
