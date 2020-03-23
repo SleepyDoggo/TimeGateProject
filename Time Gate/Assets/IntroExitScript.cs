46c033ef3714e389fe964e672bce6502bb6caa06
SleepyDoggo <akashjad1998@gmail.com>
2020-03-22 22:58:28 -0400

    Update IntroExitScript.cs
    Added the scenechange to the if statement that detects collision between player and the timegate. Changed player to Player for in the tag comparison

--- Time Gate/Assets/IntroExitScript.cs
+++ Time Gate/Assets/IntroExitScript.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroExitScript : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //double check tag is correct
-        if (collision.gameObject.CompareTag("player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             //perform exit logic
-
+            Debug.Log("Working");
+            SceneManager.LoadScene(1);
         }
     }
 }
