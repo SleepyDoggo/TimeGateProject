656d9377898e69f525b84bfbec30320e16f328f5
Michael Pascale <michaelpascal3557@gmail.com>
2020-03-23 20:43:38 -0400

    Script for aides.
    Changes which aides are present depending on whether or not the players are using controllers or the keyboard and mouse.

---
+++ Time Gate/Assets/Scripts/SwitchWhenJoysticksPresent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script requires that the input controller is present somewhere in the scene this code is running in.
+public class SwitchWhenJoysticksPresent : MonoBehaviour
+{
+    public GameObject nojoystick, joystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //assume that joysticks are not active, activate the no joystick and deactivate the other one.
+        nojoystick.SetActive(!InputController.instance.useJoySticks);
+        joystick.SetActive(InputController.instance.useJoySticks);
+
+    }
