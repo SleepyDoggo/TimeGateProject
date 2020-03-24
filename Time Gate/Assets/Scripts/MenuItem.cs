0612fe50fab29ba50a978d8717b50967214a3686
Michael Pascale <michaelpascal3557@gmail.com>
2020-03-24 19:11:23 -0400

    Menu Item
    This script controls the menu items. It is a link to the menu event associated with it, and is a wrapper for the menu. It also keeps its z rotation level.

---
+++ Time Gate/Assets/Scripts/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MenuEvent))]
+public class MenuItem : MonoBehaviour
+{
+    private MenuEvent menuEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuEvent = gameObject.GetComponent<MenuEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = transform.rotation;
+        rotation.eulerAngles = new Vector3(rotation.eulerAngles.x, rotation.eulerAngles.y, 0);
+        transform.rotation = rotation;
+    }
+
+    public void ActiveMenu()
+    {
+        transform.localScale = new Vector3(1.25f,1.25f,1);
+    }
+
+    public void DeActiveMenu()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    //This method is called by the menu when the player interacts with it. This causes it to trigger the event associated with this
+    public void Trigger()
+    {
+        menuEvent.Activate();
+    }
+}
