e3da1f5dbccc87f27b63a10eef219e4ce712a1cc
Michael Pascale <michaelpascal3557@gmail.com>
2020-03-26 18:18:01 -0400

    Menu Event for quitting game.

---
+++ Time Gate/Assets/Scripts/QuitMenuEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMenuEvent : MonoBehaviour, MenuEvent
+{
+    public void Activate()
+    {
+        Application.Quit();
+    }
+}
