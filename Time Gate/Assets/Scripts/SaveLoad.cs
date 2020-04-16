23d0e1f07c1e29e44b49c567ccc5c363211cdcda
Michael Pascale <michaelpascal3557@gmail.com>
2020-04-16 18:40:07 -0400

    Save Load functionality
    This works with the current gamedata object, or at least should once I test it out. Contains static functions for saving and loading gamedata. Does not handle the logic of using that knowledge.

---
+++ Time Gate/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /**
+     * SaveFile - takes in a gamedata object, and serializes it into the save file
+     */
+    public static void SaveFile(GameData data)
+    {
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenWrite(destination);
+        else file = File.Create(destination);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    /**
+     * LoadFile - loads the save file into a GameData object, and returns it. Returns null if file doesnt exist
+     */
+    public static GameData LoadFile()
+    {
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenRead(destination);
+        else
+        {
+            Debug.LogError("File not found");
+            return null;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        GameData data = (GameData)bf.Deserialize(file);
+        file.Close();
+
+        int currentScore = data.score;
+        return data;
+    }
+}
