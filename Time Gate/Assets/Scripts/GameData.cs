e768d6bab6b209b4af9aaef072f6aca2395d1794
Michael Pascale <michaelpascal3557@gmail.com>
2020-04-16 18:39:13 -0400

    Create GameData
    This script is responsible for packing all relevant data into one object to be written to a save file

---
+++ Time Gate/Assets/Scripts/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class GameData
+{
+    public int score;
+
+    public GameData(int score)
+    {
+        this.score = score;
+    }
+}
