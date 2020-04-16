c8f6992b2c4091cfd9ba0a3ef504ba03ee8f939e
Michael Pascale <michaelpascal3557@gmail.com>
2020-04-16 19:36:20 -0400

    Saving the game.
    This component should save the game whenever something enters its collider.

---
+++ Time Gate/Assets/Scripts/SaveGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Save();
+    }
+
+    private void Save()
+    {
+        int score = PlayerPrefs.GetInt("SCORE");
+        GameData data = new GameData(score);
+        SaveLoad.SaveFile(data);
+    }
+}
