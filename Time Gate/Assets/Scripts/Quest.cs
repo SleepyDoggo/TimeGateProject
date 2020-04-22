49ad59a8e7ec0337711b139f592f55211001f2ae
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-04-21 21:31:57 -0400

    Created Struct for quests
    This struct carries data for quests, which are loaded in and out of playerprefs along with the objectives.

---
+++ Time Gate/Assets/Scripts/Quest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Quest {
+    //name of the quest
+    public string name;
+    //represents if the quest is active
+    public bool active;
+    //is quest complete
+    public bool complete;
+    //the index in the list of the current objective
+    public int objectiveIndex;
+    //list of objectives
+    public List<QuestObjective> objectives;
+}
