2a62dfc24961dffc985c6ca4f32e0bdb1f94be47
Michael-Pascale <49280454+Michael-Pascale@users.noreply.github.com>
2020-04-26 12:21:02 -0400

    Created script for updating and saving progress for intro.
    this script updates necessary trigger events of the main quest. The first objective for the main quest to beat the level should work.

---
+++ Time Gate/Assets/IntroComplete.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroComplete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerPrefs.SetInt(SaveGame.MAIN_QUEST + " complete", 1);
+            PlayerPrefs.SetFloat(SaveGame.MAIN_QUEST + " objective 0 progress", 0f);
+            PlayerPrefs.SetInt(SaveGame.MAIN_QUEST + SaveGame.QUEST_INDEX,
+                PlayerPrefs.GetInt(SaveGame.MAIN_QUEST + SaveGame.QUEST_INDEX)+1);
+            SaveGame.Save();
+        }
+    }
+}
