a29fa7363321d4bbb96bf38bb04090f8d256158d
Sean Santorufo <seansantorufo@gmail.com>
2020-05-02 16:47:54 -0400

    prefabs and animations
    fully created animations for charger and wheels as well as creating prefabs and adding needed components

--- Time Gate/Assets/Scripts/ChargerAI.cs
+++ Time Gate/Assets/Scripts/ChargerAI.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
@@ -19,4 +19,4 @@ public class ChargerAI : MonoBehaviour, EnemyAI, Spawnable
     {
         
     }
-}
+}*/
