c090bf238288b4ee2572c6db63f423026250064f
SleepyDoggo <akashjad1998@gmail.com>
2020-05-04 03:20:31 -0400

    Menu option script for abilities
    This is to indicate selection and activation in pause menu

---
+++ Time Gate/Assets/AbilitiesMenuOption.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitiesMenuOption : MenuItem
+{
+    public override void ActiveMenu()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * 1.25f, transform.localScale.y * 1.25f, 1);
+    }
+    public override void DeActiveMenu()
+    {
+        transform.localScale = new Vector3(transform.localScale.x / 1.25f, transform.localScale.y / 1.25f, 1);
+    }
+}
